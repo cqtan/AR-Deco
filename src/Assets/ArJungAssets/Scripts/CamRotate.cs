1f860e08224f574162aba7c8d48ab0e086411f1f
CouchCat <carlos.quinto.t@gmail.com>
2017-12-01 21:17:02 +0100

    Fix static furniture problem

---
+++ src/Assets/ArJungAssets/Scripts/CamRotate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamRotate : MonoBehaviour
+{
+	private bool camAvailable;
+
+	// Use this for initialization
+	void Start ()
+	{
+		WebCamDevice[] devices = WebCamTexture.devices;
+
+		if (devices.Length == 0) {
+			Debug.Log ("no camera detected");
+			camAvailable = false;
+			return;
+		}
+		Input.gyro.enabled = true;
+		camAvailable = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!camAvailable) {
+			return;
+		}
+		Quaternion cameraRotation = new Quaternion (Input.gyro.attitude.x, Input.gyro.attitude.y, 
+			                            -Input.gyro.attitude.z, -Input.gyro.attitude.w);
+		this.transform.localRotation = cameraRotation;
+	}
+}
---
+++ src/Assets/ArJungAssets/Scripts/CorrectFurnitureRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CorrectFurnitureRotation : MonoBehaviour
+{
+	void Start ()
+	{
+		if (Application.platform == RuntimePlatform.WindowsEditor ||
+			Application.platform == RuntimePlatform.OSXEditor)
+			Debug.Log("Running on WINDOWS or OSX");
+		else 
+			this.transform.rotation = Quaternion.Euler (-90f, 90f, 0f);
+	}
+}
