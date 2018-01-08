236396a58883ab2a35550e1a2b36c7f6608aab84
CouchCat <carlos.quinto.t@gmail.com>
2018-01-08 19:41:54 +0100

    Add prototype for new Grab feature. User will be able to grab objects from any distance.

---
+++ src/Assets/ArJungAssets/Scripts/GrabOutlined.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabOutlined : MonoBehaviour {
+  [SerializeField] private ArGestureManager gesture;
+  [SerializeField] private OutlineWithRay outliner;
+  [SerializeField] private GameObject parentObject;
+  [SerializeField] private GameObject[] furnitures;
+
+  private GameObject grabbedObject;
+  private Collider collisionObject;
+  private GameObject grabPoint;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+    MoveArrow();
+
+    if (outliner.HasCollided) {
+      collisionObject = outliner.GetRayHit().collider;
+      GrabObject();
+    } else if (grabbedObject != null) {
+      grabbedObject = null;
+    }
+	}
+
+  private void GrabObject() {
+    //if (gesture.RightGrabStrength > 0.9f) {
+    if (Input.GetKey(KeyCode.G)) {
+      if (grabbedObject == null) {
+        GetGrabbedObject();
+        SetGrabPoint();
+      } else {
+        UpdateObjectPosition(grabbedObject);
+      }
+    }
+  }
+
+  private void MoveArrow() {
+    if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+      Vector3 position = this.transform.position;
+      position.x--;
+      this.transform.position = position;
+    }
+
+    if (Input.GetKeyDown(KeyCode.RightArrow)) {
+      Vector3 position = this.transform.position;
+      position.x++;
+      this.transform.position = position;
+    }
+  }
+
+  private void GetGrabbedObject() {
+    for (int i = 0; i < furnitures.Length; i++) {
+      if (IsVuMarker(collisionObject) == true) {
+        if (CheckFurnitureExists(collisionObject, furnitures[i].name)) {
+          grabbedObject = Instantiate(furnitures[i], parentObject.transform);
+          Debug.Log("Furniture CREATED!");
+        }
+      } else if (CheckCloneExists(collisionObject, furnitures[i].name)) {
+        grabbedObject = collisionObject.gameObject;
+        Debug.Log("Furniture found!");
+      }
+    }
+  }
+
+  private void SetGrabPoint() {
+    if (grabPoint == null)
+      grabPoint = new GameObject("GrabPoint");
+    grabPoint.transform.parent = this.transform.parent;
+    grabPoint.transform.position = outliner.GetRayHit().point;
+  }
+
+  private void UpdateObjectPosition(GameObject go) {
+    go.transform.position = grabPoint.transform.position;
+  }
+
+
+  /// <summary>
+  /// Checks if the VuMarker Furniture is in the list of predefined furnitures.
+  /// </summary>
+  private bool CheckFurnitureExists(Collider other, string furnitureName) {
+    return (other.gameObject.name.Substring(2) == furnitureName) ? true : false;
+  }
+
+  /// <summary>
+  /// Checks if the cloned furniture is in the list of predefined furnitures.
+  /// For grabbing objects that are already placed in the scene, e.g. "(clone)sofa".
+  /// </summary>
+  private bool CheckCloneExists(Collider other, string furnitureName) {
+    string clone = other.gameObject.name.Substring(0, other.gameObject.name.Length - 7);
+    return (clone == furnitureName) ? true : false;
+  }
+
+
+  /// <summary>
+  /// Returns true if the collided object <paramref name="col"/> comes from the VuMarker.
+  /// </summary>
+  /// <returns><c>true</c>, if vu marker was used, <c>false</c> otherwise.</returns>
+  /// <param name="col">The collided object</param>
+  private bool IsVuMarker(Collider col) {
+    string name = col.name;
+    if (name.IndexOf("Vu") > -1) return true;
+    else return false;
+  }
+
+}
---
+++ src/Assets/ArJungAssets/Scripts/OutlineWithRay.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Creates and outline around an Object that the raycast collides with.
+/// </summary>
+public class OutlineWithRay : MonoBehaviour {
+  public bool HasCollided { get; private set; }
+  public LineRenderer Line { get; set; }
+
+  [SerializeField] private bool enableLine;
+  [SerializeField] private Material MaterialOnHit;
+  [SerializeField] private Material MaterialOnNoHit;
+  [SerializeField] private Transform rayOrigin;
+  [SerializeField] string collidingTagName;
+  [SerializeField] private float min = 1.0f;
+  [SerializeField] private float max = 1.05f;
+
+  private GameObject previousGo;
+  private RaycastHit hit;
+
+  void Start () {
+    HasCollided = false;
+    createLine();
+  }
+	
+	void Update () {
+    UpdateLine();
+
+    if (RayIsColliding()) {
+      HasCollided = true;
+      ManageOutline();
+    } else {
+      HasCollided = false;
+    }
+	}
+
+  /// <summary>
+  /// Creates a ray from 'rayOrigin' and shoots out in forward direction.
+  /// <returns>
+  /// True, if ray collides with a collidable object and assigns
+  /// collision data to the 'hit' variable.
+  /// </returns>
+  /// </summary>
+  private bool RayIsColliding() {
+    Vector3 forward = rayOrigin.TransformDirection((Vector3.forward));
+    Ray ray = new Ray(rayOrigin.position, forward);
+
+    return Physics.Raycast(ray, out hit);
+  }
+
+  /// <summary>
+  /// Increases and decreases outline of collision object with the ray. 
+  /// </summary>
+  private void ManageOutline() {
+    if (hit.collider.gameObject.tag == collidingTagName) {
+      GameObject go = hit.collider.gameObject;
+      StartCoroutine(OutlineLerp(go, max, min, 0.5f));
+      previousGo = go;
+    } else {
+      if (previousGo.GetComponent<MeshRenderer>().material.GetFloat("_OutlineWidth") == max) {
+        StartCoroutine(OutlineLerp(previousGo, max, min, 0.5f));
+        previousGo = null;
+      }
+    }
+  }
+
+  private IEnumerator OutlineLerp(GameObject go, float min, float max, float time) {
+    float elapsedTime = 0;
+    
+    while (elapsedTime < time) {
+      go.GetComponent<MeshRenderer>().material.SetFloat("_OutlineWidth", Mathf.Lerp(min, max, (elapsedTime / time)));
+      elapsedTime += Time.deltaTime;
+      yield return new WaitForEndOfFrame();
+    }
+  }
+
+  private void createLine() {
+    Line = this.gameObject.AddComponent<LineRenderer>();
+    Line.material = MaterialOnNoHit;
+    Line.sortingLayerName = "OnTop";
+    Line.sortingOrder = 5;
+    Line.SetVertexCount(2);
+    Line.SetPosition(0, this.transform.position);
+    Line.SetPosition(1, this.transform.forward * 10);
+    Line.startWidth = 0.05f;
+    Line.endWidth = 0.05f;
+    Line.useWorldSpace = true;
+  }
+
+  private void UpdateLine() {
+    Line.SetPosition(0, this.transform.position);
+
+    if (HasCollided) {
+      Line.SetPosition(1, hit.point);
+      Line.material = MaterialOnHit;
+    } else {
+      Line.SetPosition(1, this.transform.forward * 10 + transform.position);
+      Line.material = MaterialOnNoHit;
+    }
+  }
+
+
+  public RaycastHit GetRayHit() { return hit; } 
+
+}
